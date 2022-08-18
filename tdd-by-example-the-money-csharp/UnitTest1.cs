d5a719d930a37c38b2c646b899349ccfdc9a3c2c
Ricky Liu <ieei.liu@gmail.com>
2022-08-18 15:18:45 +0800

    Add project files.

---
+++ tdd-by-example-the-money-csharp/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace tdd_by_example_the_money_csharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
