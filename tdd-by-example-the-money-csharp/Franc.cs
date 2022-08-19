880b419ec1aa485b1ba31b8ae6d79b4bc1f11ff2
Ricky Liu <ieei.liu@gmail.com>
2022-08-19 11:35:09 +0800

    ch5: [feat] complete Franc class by copying from Dollar class

---
+++ tdd-by-example-the-money-csharp/Franc.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+
+namespace tdd_by_example_the_money_csharp;
+
+public class Franc
+{
+    private int amount;
+
+    public Franc(int amount)
+    {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier)
+    {
+        return new Franc(amount * multiplier);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        Franc franc = (Franc) obj;
+        
+        return franc.amount == amount;
+    }
+}
--- tdd-by-example-the-money-csharp/UnitTest1.cs
+++ tdd-by-example-the-money-csharp/UnitTest1.cs
@@ -24,5 +24,13 @@ namespace tdd_by_example_the_money_csharp
             new Dollar(5).Should().Be(new Dollar(5));
             new Dollar(5).Should().NotBe(new Dollar(6));
         }
+
+        [Test]
+        public void testFrancMultiplication()
+        {
+            var five = new Franc(5);
+            five.times(2).Should().Be(new Franc(10));
+            five.times(3).Should().Be(new Franc(15));
+        }
     }
 }
