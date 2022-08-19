2cb3c65a67cfa7445977ddf502b9ea8e7f4a4872
Ricky Liu <ieei.liu@gmail.com>
2022-08-19 15:29:04 +0800

    ch6: [test] add Franc equality test cases

--- tdd-by-example-the-money-csharp/UnitTest1.cs
+++ tdd-by-example-the-money-csharp/UnitTest1.cs
@@ -23,6 +23,8 @@ namespace tdd_by_example_the_money_csharp
         {
             new Dollar(5).Should().Be(new Dollar(5));
             new Dollar(5).Should().NotBe(new Dollar(6));
+            new Franc(5).Should().Be(new Franc(5));
+            new Franc(5).Should().NotBe(new Franc(6));
         }
 
         [Test]
