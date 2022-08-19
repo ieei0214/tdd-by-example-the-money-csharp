7668e756beab2d11d8ea8808194d24be0de93da1
Ricky Liu <ieei.liu@gmail.com>
2022-08-19 16:39:07 +0800

    ch8: [refactor] adjust test case from Dollar constructor to Money.dollar() factory method

--- tdd-by-example-the-money-csharp/UnitTest1.cs
+++ tdd-by-example-the-money-csharp/UnitTest1.cs
@@ -14,18 +14,18 @@ namespace tdd_by_example_the_money_csharp
         public void testMultiplication()
         {
             Money five = Money.dollar(5);
-            five.times(2).Should().Be(new Dollar(10));
-            five.times(3).Should().Be(new Dollar(15));
+            five.times(2).Should().Be(Money.dollar(10));
+            five.times(3).Should().Be(Money.dollar(15));
         }
 
         [Test]
         public void testEquality()
         {
-            new Dollar(5).Should().Be(new Dollar(5));
-            new Dollar(5).Should().NotBe(new Dollar(6));
-            new Franc(5).Should().Be(new Franc(5));
-            new Franc(5).Should().NotBe(new Franc(6));
-            new Franc(5).Should().NotBe(new Dollar(5));
+            Money.dollar(5).Should().Be(Money.dollar(5));
+            Money.dollar(5).Should().NotBe(Money.dollar(6));
+            Money.franc(5).Should().Be(Money.franc(5));
+            Money.franc(5).Should().NotBe(Money.franc(6));
+            Money.franc(5).Should().NotBe(Money.dollar(5));
         }
 
         [Test]
