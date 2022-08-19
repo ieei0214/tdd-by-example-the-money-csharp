ad4ecb908c001e31dde472c3ec81ee20615ec8fe
Ricky Liu <ieei.liu@gmail.com>
2022-08-19 11:14:33 +0800

    ch4: [refactor] inline the temporary variable "product"

--- tdd-by-example-the-money-csharp/UnitTest1.cs
+++ tdd-by-example-the-money-csharp/UnitTest1.cs
@@ -14,10 +14,8 @@ namespace tdd_by_example_the_money_csharp
         public void testMultiplication()
         {
             var five = new Dollar(5);
-            Dollar product = five.times(2);
-            product.Should().Be(new Dollar(10));
-            product = five.times(3);
-            product.Should().Be(new Dollar(15));
+            five.times(2).Should().Be(new Dollar(10));
+            five.times(3).Should().Be(new Dollar(15));
         }
 
         [Test]
