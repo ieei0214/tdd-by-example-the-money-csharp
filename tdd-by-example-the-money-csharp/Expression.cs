7cad9c52c9572f88023d12f3b8f235675b1c149f
Ricky Liu <ieei.liu@gmail.com>
2022-08-20 22:26:08 +0800

    ch12: [make compile successfully] create Expression interface

---
+++ tdd-by-example-the-money-csharp/Expression.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_by_example_the_money_csharp
+{
+    public interface Expression
+    {
+    }
+}
