4f21b4b393d23290e294e2db4abbb2d005dbcbb9
Ricky Liu <ieei.liu@gmail.com>
2022-08-20 20:11:47 +0800

    ch9: [refactor] add a constructor parameter "currency" in Franc constructor, and got 2 compiling errors

--- tdd-by-example-the-money-csharp/Franc.cs
+++ tdd-by-example-the-money-csharp/Franc.cs
@@ -4,7 +4,7 @@ namespace tdd_by_example_the_money_csharp;
 
 public class Franc : Money
 {
-    public Franc(int amount)
+    public Franc(int amount, string currency)
     {
         this.amount = amount;
         _currency = "CHF";
