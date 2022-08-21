d1859b65af5428235526957a30e3b2d312770d04
Ricky Liu <ieei.liu@gmail.com>
2022-08-21 08:54:16 +0800

    ch14: [feat] create a private class Pair in Bank class as HashTable key

--- tdd-by-example-the-money-csharp/Bank.cs
+++ tdd-by-example-the-money-csharp/Bank.cs
@@ -18,4 +18,16 @@ public class Bank
             ? 2
             : 1);
     }
+
+    class Pair
+    {
+        private string from;
+        private string to;
+
+        public Pair(string from, string to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+    }
 }
