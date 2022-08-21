9a033e1d3a2366899e1e1fa61976780b476f8452
Ricky Liu <ieei.liu@gmail.com>
2022-08-21 08:47:26 +0800

    ch14: [experiment] create rate() of Bank class, and copying the fake implementation from Money.reduce()

--- tdd-by-example-the-money-csharp/Bank.cs
+++ tdd-by-example-the-money-csharp/Bank.cs
@@ -6,4 +6,16 @@ public class Bank
     {
         return source.reduce(this, to);
     }
+
+    public void addRate(string source, string to, int rate)
+    {
+
+    }
+
+    public int rate(string from, string to)
+    {
+        return (from.Equals("CHF") && to.Equals("USD")
+            ? 2
+            : 1);
+    }
 }
