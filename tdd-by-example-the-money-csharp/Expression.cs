6bc8a8cf71b9f6020bdf717cdc315b64d7c39123
Ricky Liu <ieei.liu@gmail.com>
2022-08-21 09:30:09 +0800

    ch15: [compile successfully] create plus() of Expression

--- tdd-by-example-the-money-csharp/Expression.cs
+++ tdd-by-example-the-money-csharp/Expression.cs
@@ -9,5 +9,6 @@ namespace tdd_by_example_the_money_csharp
     public interface Expression
     {
         public Money reduce(Bank bank, string to);
+        Expression plus(Expression addend);
     }
 }
